47039ed3fa53353cdedc592168c3fea89f1b4330
Felix de Pablo <f.depablo.lobo@gmail.com>
2022-04-07 13:50:19 +0200

    Mejora de comunicación entre padre e hijo

--- 06_ComunicacionComponentes/src/app/app.component.ts
+++ 06_ComunicacionComponentes/src/app/app.component.ts
@@ -8,5 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = '06_ComunicacionComponentes';
   textoCabecera = 'CABECERA'
-  textoPieDePagina = 'PIE DE PÁGINA'
 }
